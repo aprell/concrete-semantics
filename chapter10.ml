bd3b0569c2f0bd309008b65fb9039a5e55403267
Andreas Prell <andreas.h.prell@gmail.com>
2023-08-19 09:44:38 +0200

    Read Section 10.2

--- chapter10.ml
+++ chapter10.ml
@@ -184,7 +184,7 @@ let rec ok (xs : Vars.t) (c : command) : bool =
   | While (e, c) -> bok e && cok c
   | Skip -> true
 
-let test_ok () =
+let test_only_ivars_are_read () =
   [Imp.Examples.fib; Imp.Examples.sum]
   |> assert_property (ok (Vars.singleton "n")) ~name:"Only initialized variables are read"
 
@@ -201,9 +201,162 @@ let test_exercise_10_1 () =
     "while i < 10 { n := i + -1; i := i + 1 }";
   ] |> assert_property (exercise_10_1 Vars.empty) ~name:"Exercise 10.1"
 
+type const = name -> int option
+
+let lookup (x : name) (t : const) : int option =
+  try t x with Failure _ -> None
+
+let rec afold (e : aexpr) (t : const) : aexpr =
+  match e with
+  | Int _ -> e
+  | Var x ->
+    begin match lookup x t with
+    | Some n -> Int n
+    | None -> e
+    end
+  | Add (e1, e2) ->
+    begin match afold e1 t, afold e2 t with
+    | Int n, Int m -> Int (n + m)
+    (* Exercise 10.2 *)
+    | e', Int 0 | Int 0, e' -> e'
+    | e1', e2' -> Add (e1', e2')
+    end
+
+let approx (t : const) (s : state) (xs : name list) : bool =
+  List.for_all (fun x -> Option.fold (lookup x t) ~some:(( = ) (s x)) ~none:true) xs
+
+let afold_correct (e : aexpr) (t : const) (s : state) (xs : name list) : bool =
+  if approx t s xs then
+    aeval (afold e t) s = aeval e s
+  else true
+
+let test_afold_correct () =
+  let e = Add (Add (Var "x", Int 2), Var "y") in
+  let s = assign [("x", 1); ("y", 3)] in
+  let t = [
+    assign [("x", None); ("y", None)];
+    assign [("x", Some 1); ("y", None)];
+    assign [("x", Some 1); ("y", Some 3)] ]
+  in
+  let p = fun t -> afold_correct e t s ["x"; "y"] in
+  assert_property p t ~name:"afold_correct"
+
+(* Exercise 10.4 *)
+let rec bfold (e : bexpr) (t : const) : bexpr =
+  match e with
+  | Bool _ -> e
+  | Not e ->
+    begin match bfold e t with
+    | Bool true -> Bool false
+    | Bool false -> Bool true
+    | e' -> Not e'
+    end
+  | And (e1, e2) ->
+    begin match bfold e1 t, bfold e2 t with
+    | Bool true, Bool true -> Bool true
+    | Bool _, Bool _ -> Bool false
+    | e1', e2' -> And (e1', e2')
+    end
+  | Less (e1, e2) ->
+    begin match afold e1 t, afold e2 t with
+    | Int n, Int m when n < m -> Bool true
+    | Int _, Int _ (* when n >= m *) -> Bool false
+    | e1', e2' -> Less (e1', e2')
+    end
+
+let rec lvars (c : command) : Vars.t =
+  match c with
+  | Assign (x, _) -> Vars.singleton x
+  | Seq (c1, c2) -> Vars.union (lvars c1) (lvars c2)
+  | If (_, c1, c2) -> Vars.union (lvars c1) (lvars c2)
+  | While (_, c) -> lvars c
+  | Skip -> Vars.empty
+
+let merge (t1 : const) (t2 : const) : const =
+  fun x -> if lookup x t1 = lookup x t2 then lookup x t1 else None
+
+let restrict (t : const) (xs : Vars.t) ?(complement = false) : const =
+  if not complement then
+    fun x -> if Vars.mem x xs then lookup x t else None
+  else
+    fun x -> if Vars.mem x xs then None else lookup x t
+
+let rec defs (c : command) (t : const) : const =
+  match c with
+  | Assign (x, e) ->
+    begin match afold e t with
+    | Int n -> bind x (Some n) t
+    | _ -> bind x None t
+    end
+  | Seq (c1, c2) -> defs c1 t |> defs c2
+  | If (_, c1, c2) -> merge (defs c1 t) (defs c2 t)
+  | While (_, c) -> restrict ~complement:true t (lvars c)
+  | Skip -> t
+
+let rec fold (c : command) (t : const) : command =
+  match c with
+  | Assign (x, e) -> Assign (x, afold e t)
+  | Seq (c1, c2) -> Seq (fold c1 t, fold c2 (defs c1 t))
+  | If (e, c1, c2) -> If (e, fold c1 t, fold c2 t)
+  | While (e, c) -> While (e, fold c (restrict ~complement:true t (lvars c)))
+  | Skip -> Skip
+
+let test_fold () =
+  let c = parse "x := 42 + -5; y := x + x" in
+  match fold c empty with
+  | Seq (Assign ("x", Int 37), Assign ("y", Int 74)) -> ()
+  | _ -> failwith "test_fold"
+
+let fold_correct (c : command) (t : const) (s : state) (xs : name list) : bool =
+  if approx t s xs then
+    equivalent (ceval (fold c t) s) (ceval c s) xs
+  else true
+
+let test_fold_correct () =
+  let c = parse "x := 42 + -5; y := x + x" in
+  let p = fun c -> fold_correct c empty empty [] in
+  assert_property p [c] ~name:"fold_correct"
+
+(* Exercise 10.4 *)
+let rec fold_dce (c : command) (t : const) : command =
+  match c with
+  | Assign (x, e) -> Assign (x, afold e t)
+  | Seq (c1, c2) -> Seq (fold_dce c1 t, fold_dce c2 (defs c1 t))
+  | If (e, c1, c2) ->
+    begin match bfold e t with
+    | Bool true -> fold_dce c1 t
+    | Bool false -> fold_dce c2 t
+    | e' -> If (e', fold_dce c1 t, fold_dce c2 t)
+    end
+  | While (e, c) ->
+    begin match bfold e t with
+    | Bool false -> Skip
+    | e' -> While (e', fold_dce c (restrict ~complement:true t (lvars c)))
+    end
+  | Skip -> Skip
+
+let test_fold_dce () =
+  let c = parse "x := 42 + -5; y := x + x; if x < y { z := x + y }" in
+  match fold_dce c empty with
+  | Seq (Seq (Assign ("x", Int 37), Assign ("y", Int 74)), Assign ("z", Int 111)) -> ()
+  | _ -> failwith "test_fold_dce"
+
+let fold_dce_correct (c : command) (t : const) (s : state) (xs : name list) : bool =
+  if approx t s xs then
+    equivalent (ceval (fold_dce c t) s) (ceval c s) xs
+  else true
+
+let test_fold_dce_correct () =
+  let c = parse "x := 42 + -5; y := x + x; if x < y { z := x + y }" in
+  let p = fun c -> fold_dce_correct c empty empty [] in
+  assert_property p [c] ~name:"fold_dce_correct"
+
 let () =
   test_def_init ();
   test_well_init_programs_do_not_go_wrong ();
   test_ivars_subset_all_vars ();
-  test_ok ();
+  test_only_ivars_are_read ();
   test_exercise_10_1 ();
+  test_afold_correct ();
+  test_fold_correct ();
+  test_fold_dce_correct ();
