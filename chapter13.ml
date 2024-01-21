90bb6825642053f4ed71fca32aebcaec6a7e36bc
Andreas Prell <andreas.h.prell@gmail.com>
2024-01-21 12:10:20 +0100

    Add first example of abstract values, states, and interpretation

--- chapter13.ml
+++ chapter13.ml
@@ -200,8 +200,138 @@ let test_lemma_13_8 () =
   let p = fun c -> lemma_13_8 c s in
   assert_property p [c] ~name:"Lemma 13.8"
 
+type 'av st = (name * 'av) list
+
+module Vars = Set.Make (struct
+  type t = name
+  let compare = Stdlib.compare
+end)
+
+type parity = None | Even | Odd | Either
+
+let string_of_parity = function
+  | None -> "None"
+  | Even -> "Even"
+  | Odd -> "Odd"
+  | Either -> "Either"
+
+let assoc x xs =
+  List.assoc_opt x xs
+  |> Option.value ~default:None
+
+let order_parity a b = a = None || a = b || b = Either
+
+let order_parity' a b =
+  let names = Vars.of_list (fst (List.split a) @ fst (List.split b)) in
+  Vars.fold (fun x s ->
+      let av, av' = assoc x a, assoc x b in
+      (x, order_parity av av') :: s
+    ) names []
+  |> List.for_all snd
+
+let join_parity a b =
+  match a, b with
+  | None, a
+  | a, None -> a
+  | _ when a = b -> a
+  | _ -> Either
+
+let join_parity' a b =
+  let names = Vars.of_list (fst (List.split a) @ fst (List.split b)) in
+  Vars.fold (fun x s ->
+      let av, av' = assoc x a, assoc x b in
+      (x, join_parity av av') :: s
+    ) names []
+
+(* To be generalized later *)
+
+let rec aeval_parity (e : aexpr) (s : parity st) : parity =
+  match e with
+  | Int n -> num_parity n
+  | Var x -> List.assoc x s
+  | Add (e1, e2) -> plus_parity (aeval_parity e1 s) (aeval_parity e2 s)
+
+and num_parity n = if n mod 2 = 0 then Even else Odd
+
+and plus_parity a b =
+  match a, b with
+  | Even, Even
+  | Odd, Odd -> Even
+  | Even, Odd
+  | Odd, Even -> Odd
+  | Either, _
+  | _, Either -> Either
+  | _ -> assert false
+
+let asem_parity x e s =
+  match List.assoc_opt x s with
+  | Some _ -> (x, aeval_parity e s) :: s
+  | None -> (x, None) :: s
+
+let bsem_parity _e s = s
+
+let rec step_parity
+  (s : parity st)
+  (c : parity st Annotated.command)
+     : parity st Annotated.command =
+  match c with
+  | Annotated.Assign (x, e, _) ->
+    Annotated.Assign (x, e, asem_parity x e s)
+  | Annotated.Seq (c1, c2) ->
+    Annotated.Seq (step_parity s c1, step_parity (post_annotation c1) c2)
+  | Annotated.If (e, p1, c1, p2, c2, _) ->
+    let p1', p2' = bsem_parity e s, bsem_parity (Not e) s in 
+    let q' = join_parity' (post_annotation c1) (post_annotation c2) in
+    Annotated.If (e, p1', step_parity p1 c1, p2', step_parity p2 c2, q')
+  | Annotated.While (i, e, p, c, _) ->
+    let i' = join_parity' s (post_annotation c) in
+    let p', q' = bsem_parity e i, bsem_parity (Not e) i in
+    Annotated.While (i', e, p', step_parity p c, q')
+  | Annotated.Skip _ ->
+    Annotated.Skip s
+
+let rec while_ b f x =
+  if b x then while_ b f (f x) else x
+
+let pfp order f s =
+  while_ (fun x -> not (order (f x) x)) f s
+
+let bot (c : command) =
+  annotate (function _ -> []) c
+
+let abs_interp_parity (c : command) (s : parity st) =
+  pfp (fun x y ->
+      let sx = annotations x in
+      let sy = annotations y in
+      List.for_all2 order_parity' sx sy
+    ) (step_parity s) (bot c)
+
+let show_parity (xs : name list) (s : parity st) : string =
+  List.map (fun x -> Printf.sprintf "%s := %s" x (List.assoc x s |> string_of_parity)) xs
+  |> String.concat ", "
+  |> Printf.sprintf "%s"
+
+let test_abs_interp_parity () =
+  let c = parse "x := 3; while x < 10 { x := x + 2 }" in
+  let s = [("x", Either)] in
+  let ai = Printf.sprintf "\n{%s}\n%s\n"
+    (show_parity ["x"] s)
+    (Annotated.pp_command (show_parity ["x"]) (abs_interp_parity c s))
+  in
+  assert (ai = {|
+{x := Either}
+x := 3 {x := Odd};
+{x := Odd}
+while x < 10 {
+  {x := Odd}
+  x := x + 2 {x := Odd}
+}
+{x := Odd}
+|})
+
 let () =
   test_collecting_semantics_1 ();
   test_collecting_semantics_2 ();
   test_collecting_semantics_3 ();
   test_lemma_13_8 ();
+  test_abs_interp_parity ();
